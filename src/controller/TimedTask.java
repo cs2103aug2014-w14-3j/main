aae277732917815ed5c5da8ea324f43ffede4bb3
XianHui <xianhui.koh@gmail.com>
2014-09-29 23:52:48 +0800

    add TimedTask class for timed tasks

---
+++ src/controller/TimedTask.java
@@ -0,0 +1,60 @@
+package controller;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+class TimedTask implements Task{
+	int LENGTH_OF_TIME = 9;
+	String[] wordsInString;
+	String task;
+	String dateTime;
+		
+	TimedTask(String command) {
+		wordsInString = command.split(" ");
+		int numOfWordsInString = wordsInString.length;
+		dateTime = wordsInString[numOfWordsInString - 2] + " " + wordsInString[numOfWordsInString - 1];
+		
+		if(isPrioritized()) {
+			task = command.substring(1, command.length() - 9);
+		} else {
+			task = command.substring(0, command.length() - 9);
+		}
+	}
+		
+	public Date getDateTime() {
+		try {
+			SimpleDateFormat dateTimeFormat = new SimpleDateFormat("HHHH HHHH");
+			return dateTimeFormat.parse(dateTime);
+		} catch (ParseException e) {
+		}
+		
+		return null;
+	}
+		
+	public String getDesc() {
+		return task;
+	}
+		
+	public Boolean isPrioritized() {
+		if(task.substring(0, 1).equals("!")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+		
+	public String toString() {
+		return task;
+	}
+
+	@Override
+	public Boolean isOverdue() {
+		return false;
+	}
+
+	@Override
+	public controller.Task.TaskType getType() {
+		return controller.Task.TaskType.TIMED;
+	}
+}
