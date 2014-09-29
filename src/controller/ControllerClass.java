5f07a177028a0b85b8235e27ac96a01ed2089349
G. Vishnu Priya <vishki.priya@gmail.com>
2014-09-29 23:32:05 +0800

    Implements controller interface

--- src/controller/ControllerClass.java
+++ src/controller/ControllerClass.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
  * 
  */
 
-public class ControllerClass {
+public class ControllerClass implements Controller {
 	
 	enum CommandType {
 		ADD, DELETE, EDIT, DISPLAY, INVALID
