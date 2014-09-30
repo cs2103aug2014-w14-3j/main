ef8b69310a46a9064fd316a9157a06b831968454
marksh8 <marksh8@gmail.com>
2014-09-30 11:29:37 +0800

    merge from master& add gui entry (not working)

---
+++ src/ui/GraphicalUI.java
@@ -0,0 +1,65 @@
+/**
+ * 
+ */
+package ui;
+
+import java.util.ArrayList;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Luo Shaohuai
+ *
+ */
+public class GraphicalUI extends Application implements UI  {
+	
+	/* (non-Javadoc)
+	 * @see javafx.application.Application#start(javafx.stage.Stage)
+	 */
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see ui.UI#display(java.util.ArrayList)
+	 */
+	@Override
+	public void display(ArrayList<String> page) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see ui.UI#updateToScreen()
+	 */
+	@Override
+	public void updateToScreen() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see ui.UI#initialize()
+	 */
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see ui.UI#exit()
+	 */
+	@Override
+	public void exit() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	ArrayList<String> displayBuf;
+
+}
