567c48a70691b34a6d08c4b81abdc846c770cf78
G. Vishnu Priya <vishki.priya@gmail.com>
2014-09-30 19:45:04 +0800

    Added comments for methods

--- src/controller/ControllerClass.java
+++ src/controller/ControllerClass.java
@@ -25,23 +25,27 @@ public class ControllerClass implements Controller {
 		this.ui = ui;
 	}
 	
+	//This public method calls the parser to begin processing the user input.
 	public void execCmd(String command) {
 			parseCommand(command);
 		}
 	
+	//This method gets the command type of user input and further processes the input.
 	private void parseCommand(String command) {
 			String operation = getOperation(command);
 			CommandType commandType = matchCommandType(operation);
 			String content = removeCommandType(command, operation);
 			processInput(commandType, content);
 		}
-
+	
+	//This method returns the type of operation to be carried out, either add, delete, edit or display.
 	public String getOperation(String command) {
 		String[] splitCommandIntoWords = command.split(" ");
 		String operation = splitCommandIntoWords[POSITION_OF_OPERATION];
 		return operation;
 	}
-		
+	
+	//This method processes each string of user input according to the command type.
 	private void processInput(CommandType commandType, String content) {
 		switch (commandType) {
 		case ADD:
@@ -79,11 +83,13 @@ public class ControllerClass implements Controller {
 	private void addTask(String content) {
 		
 	}
-
+	
+	//This method removes the command, either add, delete, edit or display, from the command string.
 	private String removeCommandType(String command, String operation) {
 		return command.replace(operation,"").trim();
 	}
-
+	
+	//This method returns the command type for each operation.
 	private	CommandType matchCommandType (String operation) {
 			if (operation.equalsIgnoreCase("add")) {
 				return CommandType.ADD;
