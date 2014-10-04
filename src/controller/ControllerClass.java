dd21bda81c23f4da89c89aea994fbd5294fd62b5
marksh8 <marksh8@gmail.com>
2014-10-04 13:59:18 +0800

    marged ControllerClass in master

--- src/controller/ControllerClass.java
+++ src/controller/ControllerClass.java
@@ -1,10 +1,12 @@
 package controller;
 
+import storage.Storage;
+import storage.StoragePlus;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.HashMap;
 
@@ -15,66 +17,93 @@ import java.util.HashMap;
  */
 
 public class ControllerClass implements Controller {
-	
+
 	enum CommandType {
 		ADD, DELETE, EDIT, DISPLAY, INVALID
 	};
-	
+
 	private static final int POSITION_OF_OPERATION = 0;
 	private static final int numTasksInSinglePage = 10;
-	private static final Map<String, String> DATE_FORMAT_REGEXPS = new HashMap<String, String>() {{
-	    put("^\\d{8}$", "yyyyMMdd");
-	    put("^\\d{1,2}-\\d{1,2}-\\d{4}$", "dd-MM-yyyy");
-	    put("^\\d{4}-\\d{1,2}-\\d{1,2}$", "yyyy-MM-dd");
-	    put("^\\d{1,2}/\\d{1,2}/\\d{4}$", "MM/dd/yyyy");
-	    put("^\\d{4}/\\d{1,2}/\\d{1,2}$", "yyyy/MM/dd");
-	    put("^\\d{1,2}\\s[a-z]{3}\\s\\d{4}$", "dd MMM yyyy");
-	    put("^\\d{1,2}\\s[a-z]{4,}\\s\\d{4}$", "dd MMMM yyyy");
-	    put("^\\d{12}$", "yyyyMMddHHmm");
-	    put("^\\d{8}\\s\\d{4}$", "yyyyMMdd HHmm");
-	    put("^\\d{1,2}-\\d{1,2}-\\d{4}\\s\\d{1,2}:\\d{2}$", "dd-MM-yyyy HH:mm");
-	    put("^\\d{4}-\\d{1,2}-\\d{1,2}\\s\\d{1,2}:\\d{2}$", "yyyy-MM-dd HH:mm");
-	    put("^\\d{1,2}/\\d{1,2}/\\d{4}\\s\\d{1,2}:\\d{2}$", "MM/dd/yyyy HH:mm");
-	    put("^\\d{4}/\\d{1,2}/\\d{1,2}\\s\\d{1,2}:\\d{2}$", "yyyy/MM/dd HH:mm");
-	    put("^\\d{1,2}\\s[a-z]{3}\\s\\d{4}\\s\\d{1,2}:\\d{2}$", "dd MMM yyyy HH:mm");
-	    put("^\\d{1,2}\\s[a-z]{4,}\\s\\d{4}\\s\\d{1,2}:\\d{2}$", "dd MMMM yyyy HH:mm");
-	    put("^\\d{14}$", "yyyyMMddHHmmss");
-	    put("^\\d{8}\\s\\d{6}$", "yyyyMMdd HHmmss");
-	    put("^\\d{1,2}-\\d{1,2}-\\d{4}\\s\\d{1,2}:\\d{2}:\\d{2}$", "dd-MM-yyyy HH:mm:ss");
-	    put("^\\d{4}-\\d{1,2}-\\d{1,2}\\s\\d{1,2}:\\d{2}:\\d{2}$", "yyyy-MM-dd HH:mm:ss");
-	    put("^\\d{1,2}/\\d{1,2}/\\d{4}\\s\\d{1,2}:\\d{2}:\\d{2}$", "MM/dd/yyyy HH:mm:ss");
-	    put("^\\d{4}/\\d{1,2}/\\d{1,2}\\s\\d{1,2}:\\d{2}:\\d{2}$", "yyyy/MM/dd HH:mm:ss");
-	    put("^\\d{1,2}\\s[a-z]{3}\\s\\d{4}\\s\\d{1,2}:\\d{2}:\\d{2}$", "dd MMM yyyy HH:mm:ss");
-	    put("^\\d{1,2}\\s[a-z]{4,}\\s\\d{4}\\s\\d{1,2}:\\d{2}:\\d{2}$", "dd MMMM yyyy HH:mm:ss");
-	}};
+	private static final Map<String, String> DATE_FORMAT_REGEXPS = new HashMap<String, String>() {
+		{
+			put("^\\d{8}$", "yyyyMMdd");
+			put("^\\d{1,2}-\\d{1,2}-\\d{4}$", "dd-MM-yyyy");
+			put("^\\d{4}-\\d{1,2}-\\d{1,2}$", "yyyy-MM-dd");
+			put("^\\d{1,2}/\\d{1,2}/\\d{4}$", "MM/dd/yyyy");
+			put("^\\d{4}/\\d{1,2}/\\d{1,2}$", "yyyy/MM/dd");
+			put("^\\d{1,2}\\s[a-z]{3}\\s\\d{4}$", "dd MMM yyyy");
+			put("^\\d{1,2}\\s[a-z]{4,}\\s\\d{4}$", "dd MMMM yyyy");
+			put("^\\d{12}$", "yyyyMMddHHmm");
+			put("^\\d{8}\\s\\d{4}$", "yyyyMMdd HHmm");
+			put("^\\d{1,2}-\\d{1,2}-\\d{4}\\s\\d{1,2}:\\d{2}$",
+					"dd-MM-yyyy HH:mm");
+			put("^\\d{4}-\\d{1,2}-\\d{1,2}\\s\\d{1,2}:\\d{2}$",
+					"yyyy-MM-dd HH:mm");
+			put("^\\d{1,2}/\\d{1,2}/\\d{4}\\s\\d{1,2}:\\d{2}$",
+					"MM/dd/yyyy HH:mm");
+			put("^\\d{4}/\\d{1,2}/\\d{1,2}\\s\\d{1,2}:\\d{2}$",
+					"yyyy/MM/dd HH:mm");
+			put("^\\d{1,2}\\s[a-z]{3}\\s\\d{4}\\s\\d{1,2}:\\d{2}$",
+					"dd MMM yyyy HH:mm");
+			put("^\\d{1,2}\\s[a-z]{4,}\\s\\d{4}\\s\\d{1,2}:\\d{2}$",
+					"dd MMMM yyyy HH:mm");
+			put("^\\d{14}$", "yyyyMMddHHmmss");
+			put("^\\d{8}\\s\\d{6}$", "yyyyMMdd HHmmss");
+			put("^\\d{1,2}-\\d{1,2}-\\d{4}\\s\\d{1,2}:\\d{2}:\\d{2}$",
+					"dd-MM-yyyy HH:mm:ss");
+			put("^\\d{4}-\\d{1,2}-\\d{1,2}\\s\\d{1,2}:\\d{2}:\\d{2}$",
+					"yyyy-MM-dd HH:mm:ss");
+			put("^\\d{1,2}/\\d{1,2}/\\d{4}\\s\\d{1,2}:\\d{2}:\\d{2}$",
+					"MM/dd/yyyy HH:mm:ss");
+			put("^\\d{4}/\\d{1,2}/\\d{1,2}\\s\\d{1,2}:\\d{2}:\\d{2}$",
+					"yyyy/MM/dd HH:mm:ss");
+			put("^\\d{1,2}\\s[a-z]{3}\\s\\d{4}\\s\\d{1,2}:\\d{2}:\\d{2}$",
+					"dd MMM yyyy HH:mm:ss");
+			put("^\\d{1,2}\\s[a-z]{4,}\\s\\d{4}\\s\\d{1,2}:\\d{2}:\\d{2}$",
+					"dd MMMM yyyy HH:mm:ss");
+		}
+	};
 
-	
 	private ArrayList<Task> tasks;
+	private ArrayList<String> tasksString;
+
+	// This method starts execution of each user command by first retrieving
+	// all existing tasks stored and goes on to parse user command, to determine
+	// which course of action to take.
+	public void execCmd(String command) {
+		getFileContent();
+		parseCommand(command);
+	}
+
+	// This method returns all the existing tasks in the list, if any.
+	private void getFileContent() {
+		Storage storage = new StoragePlus();
+		tasksString = storage.read();
+	}
 	
-	//deleted constructor
-	//Changed return value, added return null 
-	//Update needed (TO-DO)
-	public ArrayList<String> execCmd(String command) {
-			parseCommand(command);
-			return null;
-		}
-	
-	//This method gets the command type of user input and further processes the input.
+	private void convertStringsToTasks() {
+		
+	}
+
+	// This method gets the command type of user input and further processes the
+	// input.
 	private void parseCommand(String command) {
-			String operation = getOperation(command);
-			CommandType commandType = matchCommandType(operation);
-			String content = removeCommandType(command, operation);
-			processInput(commandType, content);
-		}
-	
-	//This method returns the type of operation to be carried out, either add, delete, edit or display.
+		String operation = getOperation(command);
+		CommandType commandType = matchCommandType(operation);
+		String content = removeCommandType(command, operation);
+		processInput(commandType, content);
+	}
+
+	// This method returns the type of operation to be carried out, either add,
+	// delete, edit or display.
 	public String getOperation(String command) {
 		String[] splitCommandIntoWords = command.split(" ");
 		String operation = splitCommandIntoWords[POSITION_OF_OPERATION];
 		return operation;
 	}
-	
-	//This method processes each string of user input according to the command type.
+
+	// This method processes each string of user input according to the command
+	// type.
 	private void processInput(CommandType commandType, String content) {
 		switch (commandType) {
 		case ADD:
@@ -89,33 +118,33 @@ public class ControllerClass implements Controller {
 			display();
 			break;
 		case INVALID:
-			//printInvalidCommand();
+			// printInvalidCommand();
 			break;
 		default:
-			//throw new Error();
+			// throw new Error();
+		}
 	}
-	}
-
 
 	private void display() {
 
 	}
 
 	private void editTask(String content) {
-		
+
 	}
 
 	private void deleteTask(String content) {
-		
+
 	}
 
 	private void addTask(String content) {
 		Task task = processUserInput(content);
 		this.tasks.add(task);
 	}
-	
+
 	/**
 	 * TODO
+	 * 
 	 * @author Luo Shaohuai
 	 * @param content
 	 * @return Task Object
@@ -127,26 +156,27 @@ public class ControllerClass implements Controller {
 			String format = determineDateFormat(word);
 			if (format != null) {
 				SimpleDateFormat dateFormat = new SimpleDateFormat(format);
-		 		try {
-		 			Date date = dateFormat.parse(word);
-		 			if (date != null) {
-		 				dates.add(date);
-		 			}
+				try {
+					Date date = dateFormat.parse(word);
+					if (date != null) {
+						dates.add(date);
+					}
 				} catch (ParseException e) {
-					//do nothing
+					// do nothing
 				}
 			}
 		}
-		
+
 		if (dates.size() > 2) {
-			dates = new ArrayList<Date>(dates.subList(dates.size() - 2, dates.size()));
+			dates = new ArrayList<Date>(dates.subList(dates.size() - 2,
+					dates.size()));
 		}
-		
+
 		boolean priority = false;
-		if(content.contains("!")) {
+		if (content.contains("!")) {
 			priority = true;
 		}
-		
+
 		Task task = null;
 		if (dates.size() == 0) {
 			task = new FloatingTask(priority, content);
@@ -155,45 +185,50 @@ public class ControllerClass implements Controller {
 		} else if (dates.size() == 2) {
 			task = new TimedTask(priority, content, dates.get(0), dates.get(1));
 		}
-		
+
 		return task;
 	}
-	
-	
+
 	/**
-	 * @author Retrieved from http://stackoverflow.com/questions/3389348/parse-any-date-in-java by Luo Shaohuai
-	 * Determine SimpleDateFormat pattern matching with the given date string. Returns null if
-	 * format is unknown. You can simply extend DateUtil with more formats if needed.
-	 * @param dateString The date string to determine the SimpleDateFormat pattern for.
-	 * @return The matching SimpleDateFormat pattern, or null if format is unknown.
+	 * @author Retrieved from
+	 *         http://stackoverflow.com/questions/3389348/parse-any-date-in-java
+	 *         by Luo Shaohuai Determine SimpleDateFormat pattern matching with
+	 *         the given date string. Returns null if format is unknown. You can
+	 *         simply extend DateUtil with more formats if needed.
+	 * @param dateString
+	 *            The date string to determine the SimpleDateFormat pattern for.
+	 * @return The matching SimpleDateFormat pattern, or null if format is
+	 *         unknown.
 	 * @see SimpleDateFormat
 	 */
 	private static String determineDateFormat(String dateString) {
-	    for (String regexp : DATE_FORMAT_REGEXPS.keySet()) {
-	        if (dateString.toLowerCase().matches(regexp)) {
-	            return DATE_FORMAT_REGEXPS.get(regexp);
-	        }
-	    }
-	    return null; // Unknown format.
-	}
-	
-	//This method removes the command, either add, delete, edit or display, from the command string.
-	private String removeCommandType(String command, String operation) {
-		return command.replace(operation,"").trim();
-	}
-	
-	//This method returns the command type for each operation.
-	private	CommandType matchCommandType (String operation) {
-			if (operation.equalsIgnoreCase("add")) {
-				return CommandType.ADD;
-			} else if (operation.equalsIgnoreCase("delete")) {
-				return CommandType.DELETE;
-			} else if (operation.equalsIgnoreCase("edit")){
-				return CommandType.EDIT;
-			} else if ((operation.equalsIgnoreCase("display")) || (operation.equalsIgnoreCase("list"))) {
-				return CommandType.DISPLAY;
-			} else {
-				return CommandType.INVALID;
+		for (String regexp : DATE_FORMAT_REGEXPS.keySet()) {
+			if (dateString.toLowerCase().matches(regexp)) {
+				return DATE_FORMAT_REGEXPS.get(regexp);
 			}
 		}
+		return null; // Unknown format.
 	}
+
+	// This method removes the command, either add, delete, edit or display,
+	// from the command string.
+	private String removeCommandType(String command, String operation) {
+		return command.replace(operation, "").trim();
+	}
+
+	// This method returns the command type for each operation.
+	private CommandType matchCommandType(String operation) {
+		if (operation.equalsIgnoreCase("add")) {
+			return CommandType.ADD;
+		} else if (operation.equalsIgnoreCase("delete")) {
+			return CommandType.DELETE;
+		} else if (operation.equalsIgnoreCase("edit")) {
+			return CommandType.EDIT;
+		} else if ((operation.equalsIgnoreCase("display"))
+				|| (operation.equalsIgnoreCase("list"))) {
+			return CommandType.DISPLAY;
+		} else {
+			return CommandType.INVALID;
+		}
+	}
+}
