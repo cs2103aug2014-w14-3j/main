b01ee34531b9ce7cc42a482df41188d1841cb75e
G. Vishnu Priya <vishki.priya@gmail.com>
2014-10-22 22:35:49 +0800

    Added unit tests to test delete functionality

---
+++ src/controller/controllerClassTestVishnu.java
@@ -0,0 +1,66 @@
+package controller;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit tests for controller class for delete functionality 
+ * @author G. Vishnu Priya
+ *
+ */
+public class controllerClassTestVishnu {
+	
+	Controller controller;
+	ArrayList<String> taskDisplayList;
+	
+	@Before
+	public void initialize() {
+		controller = new ControllerClass();
+	}
+	
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
+	public String getDescription(String displayString) {
+		String[] splitDisplayString = displayString.split("%");
+		return splitDisplayString[1];
+	}
+	
+	@Test
+	//This is a boundary case for the positive values within the range of task numbers, of the partition.
+	public void testExecuteDeletePostiveValue() throws Exception {
+		controller.execCmd("add have lunch at school");
+		controller.execCmd("add meet friends for movie");
+		controller.execCmd("delete 1");
+		controller.execCmd("list");
+		taskDisplayList = controller.getCurrentList();
+		
+		assertEquals("meet friends for movie", getDescription(taskDisplayList.get(0)));
+		
+		controller.execCmd("delete 1");
+	}
+	
+	@Test
+	//This is a case around the boundary for the values outside the range of task numbers, of the partition.
+	public void testExecuteDeleteNegativeValue() throws Exception {
+		controller.execCmd("add have lunch at school");
+		controller.execCmd("add meet friends for movie");
+	
+		try {
+			controller.execCmd("delete -1");
+			fail("Should throw an Exception when negative numbers are given as input.");
+		} catch (Exception e) {
+			assertEquals("Task does not exist. Please enter task number within the range.", e.getMessage());
+			controller.execCmd("delete 2");
+			controller.execCmd("delete 1");
+		}
+	}
+	
+	
+}
