ff5c7549ed2f70456d919e4b318c699e36a2e98b
marksh8 <marksh8@gmail.com>
2014-11-02 15:53:20 +0800

    change structure of ui

--- src/ui/UIControl.java
+++ src/ui/UIControl.java
@@ -42,7 +42,8 @@ public class UIControl extends BorderPane {
 	
 	private Popup noti;
 	private Popup suggest;
-	PopupListControl popupList;
+	PopupList popupList;
+	Noti notiBox;
 	
 	private double mouseX;
 	private double mouseY;
@@ -50,9 +51,11 @@ public class UIControl extends BorderPane {
 	public UIControl() {
 		displayCurTime();
 		noti = new Popup();	
+		notiBox = new Noti();
 		suggest = new Popup();
-		popupList = new PopupListControl();
+		popupList = new PopupList();
 		suggest.getContent().add(popupList.getPane());
+		noti.getContent().add(notiBox.getBox());
 	}
 	
 	public void init() {
@@ -91,8 +94,7 @@ public class UIControl extends BorderPane {
 	}
 	
 	public void showNoti(String message) {
-		noti.getContent().clear();
-		noti.getContent().add(new Text(message));
+		notiBox.setText(message);
 		noti.setHeight(20.0);
 		
 		double posX = input.localToScene(0.0, 0.0).getX() 
