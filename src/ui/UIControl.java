50a9d2b47578f8878c916fb483650d0383e99345
marksh8 <marksh8@gmail.com>
2014-11-05 13:35:48 +0800

    fix noti pos

--- src/ui/UIControl.java
+++ src/ui/UIControl.java
@@ -54,6 +54,11 @@ public class UIControl extends BorderPane {
 		noti = new Popup();	
 		notiBox = new Noti();
 		noti.getContent().add(notiBox.getBox());
+		noti.setAutoFix(false);
+		noti.setAutoHide(true);
+		noti.setConsumeAutoHidingEvents(false);
+		noti.setHideOnEscape(true);
+		noti.setAnchorLocation(AnchorLocation.CONTENT_TOP_LEFT);
 		
 		suggest = new Popup();
 		popupList = new PopupList();
@@ -154,7 +159,6 @@ public class UIControl extends BorderPane {
 			double posY = input.getScene().getWindow().getY()
 					+ input.localToScene(0.0, 0.0).getY()
 					+ input.getHeight();
-			suggest.setAutoFix(false);
 			
 			if (popupList.loadList(value.getList(newString))) {
 				suggest.show(input, posX, posY);
