f0cee6bcc8602ced2085dfde472ec51c4789d83f
XianHui <xianhui.koh@gmail.com>
2014-11-06 22:33:43 +0800

    added comments for Pair

--- src/controller/Pair.java
+++ src/controller/Pair.java
@@ -1,18 +1,41 @@
 package controller;
 
+/**
+ * Class for pairs of integers.
+ */
+//@author
 public class Pair {
 	int first;
 	int second;
 	
+	/**
+	 * Constructs a pair of integers.
+	 * 
+	 * @param _first	First integer of the pair.
+	 * @param _second	Second integer of the pair.
+	 */
+	//@author
 	public Pair( int _first, int _second){
 		first=_first;
 		second=_second;
 	}
 	
+	/**
+	 * Gets the first integer of the pair.
+	 * 
+	 * @return	First integer of the pair.
+	 */
+	//@author
 	public int getFirst(){
 		return first;
 	}
 	
+	/**
+	 * Gets the second integer of the pair.
+	 * 
+	 * @return	Second integer of the pair.
+	 */
+	//@author
 	public int getSecond(){
 		return second;
 	}
