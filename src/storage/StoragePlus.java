376b2894f980ddc82e6b0cb9eef19fb3eec0ef0a
XianHui <xianhui.koh@gmail.com>
2014-11-07 09:18:38 +0800

    added comments for Storage

--- src/storage/StoragePlus.java
+++ src/storage/StoragePlus.java
@@ -7,6 +7,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * Storage class to create a Storage object for storing tasks.
+ */
+//@author
 public class StoragePlus implements Storage {
 	// Name of the file to store the list
 	private final static String fileName = "ToDoList.txt";
@@ -16,6 +20,12 @@ public class StoragePlus implements Storage {
 	
 	private final static String archiveFile="Archive.txt";
 
+	/**
+	 * Reads from the storage and returns a stringed task list for the main list.
+	 * 
+	 * @return	List of stringed tasks.
+	 */
+	//@author
 	public List<String> read() {
 
 		List<String> toDoList = new ArrayList<String>();
@@ -40,7 +50,12 @@ public class StoragePlus implements Storage {
 		return toDoList;
 	}
 	
-	
+	/**
+	 * Reads from the storage and returns a stringed task list for the archive list.
+	 * 
+	 * @return List of stringed tasks.
+	 */
+	//@author
 	public List<String> readArchive(){
 		
 		List<String> archiveList=new ArrayList<String>();
@@ -64,9 +79,13 @@ public class StoragePlus implements Storage {
 		return archiveList;
 	}
 
-	// before write to the list, move the content of current list to
-	// "ToDoList.backup"
-
+	/**
+	 * Writes main list into storage.
+	 * Before writing to the list, move the content of current list to the backup.
+	 * 
+	 * @param list	List of stringed tasks.
+	 */
+	//@author
 	public void write(List<String> list) {
 
 		File backUp = new File(backUpFile);
@@ -113,7 +132,12 @@ public class StoragePlus implements Storage {
 	}
 	
 	
-	//wrtie the archive list to the storage file
+	/**
+	 * Writes archive list into storage.
+	 * 
+	 * @param archiveList	List of completed stringed tasks.
+	 */
+	//@author
 	public void writeArchive(List<String> archiveList){
 		
 		File archive=new File(archiveFile);
