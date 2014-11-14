7163e28a2da318e979eace006c17c389c5f0fb05
thientran1707 <thientran1707@gmail.com>
2014-11-14 13:06:11 +0800

    test

--- src/tests/TestSearch.java
+++ src/tests/TestSearch.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import controller.Controller;
@@ -12,25 +13,40 @@ import controller.ControllerClass;
 //@author A0112044B
 public class TestSearch {
 
-	
+	Controller controller;
+	List<String> list;
 
 	public String getDesc(String task) {
 		String[] para=task.split("%");
 		return para[0];
 	}
 	
+	@Before
+	public void setup() throws Exception {
+		controller = ControllerClass.getInstance();
+	
+	
+	}
+	
 	@Test
 	//Test exactSearch: the keyword will appear exactly in one of the description of the tasks
 	//return the task list
 	public void tesExactSearch() throws Exception{
-		Controller controller=ControllerClass.getInstance();
-		List<String> list;
+		
+		controller.execCmd("add go home");
+		controller.execCmd("add go to school");
+		controller.execCmd("add \"do homework and sleep\" nov 20 3pm");
+		controller.execCmd("add \"have lunch\" nov 18");
+		controller.execCmd("add \"do homework\" today 3pm");
+		controller.execCmd("add \"get up early\" tomorrow 7am");
 		//search for "go home"
 		controller.execCmd("go home");
 		
 		list = controller.getCurrentList();
+		
+		assertEquals(getDesc(list.get(0)),"5. go home");
 	
-		assertEquals(getDesc(list.get(0)),"10. go home");
+	
 	}	
 	
 	
@@ -38,14 +54,17 @@ public class TestSearch {
 	//Test exactSearch
 	//the keyword is typed wrongly and the software will give the list of the tasks which are nearly matched
 	public void testNearMatchSearch() throws Exception{
-		Controller controller=ControllerClass.getInstance();
-		List<String> list;
-		//search for "eat lunch" but user types it as "ate lunchh";
-		controller.execCmd("lunchh");
 		
+		//search for "eat lunch" but user types it as "ate lunchh";
+		
+		
+		controller.execCmd("lunnch");
 		list = controller.getCurrentList();
+		
+		assertEquals(getDesc(list.get(0)),"3. have lunch");
+		
+		
 	
-		assertEquals(getDesc(list.get(0)),"17. go to school and eat lunch");
 	}	
 	
 	
@@ -53,14 +72,16 @@ public class TestSearch {
 	@Test
 	//Test search on a day
 	public void testSearchDate() throws Exception{
-		Controller controller=ControllerClass.getInstance();
-		List<String> list;
 		
+		
+	
 		controller.execCmd("today");
 		
 		list = controller.getCurrentList();
 	
-		assertEquals(getDesc(list.get(0)),"2. finish the code");
+	
+		assertEquals(getDesc(list.get(0)),"1. do homework");
+	
 	}	
 	
 	
@@ -70,14 +91,16 @@ public class TestSearch {
 	//Test search by a day
 
 	public void testSearchByDate() throws Exception{
-		Controller controller=ControllerClass.getInstance();
-		List<String> list;
+	
+	
 	
 		controller.execCmd("by tomorrow");
 		
 		list = controller.getCurrentList();
-	
-		assertEquals(getDesc(list.get(0)),"1. do the homework");
+		
+		assertEquals(getDesc(list.get(0)),"1. do homework");
+		assertEquals(getDesc(list.get(1)),"2. get up early");
+		
 	}	
 	
 	
@@ -86,14 +109,19 @@ public class TestSearch {
 	//Test search a period
 
 	public void testSearchPeriod() throws Exception{
-		Controller controller=ControllerClass.getInstance();
-		List<String> list;
 	
-		controller.execCmd("tomorrow to nov 15");
+	
+	
+		
+		controller.execCmd("nov 18 to nov 21");
 		
 		list = controller.getCurrentList();
-	
-		assertEquals(getDesc(list.get(0)),"6. deadline for MA2213 lab");
+		
+
+		assertEquals(getDesc(list.get(0)),"3. have lunch");
+		assertEquals(getDesc(list.get(1)),"4. do homework and sleep");
+		
+		
 	}
 	
 	
@@ -102,20 +130,15 @@ public class TestSearch {
 	//Test search a period
 
 	public void testDescAndDate() throws Exception{
-		Controller controller=ControllerClass.getInstance();
-		List<String> list;
-	
-		controller.execCmd("\"go\" nov 15");
 		
+		
+		controller.execCmd(" \"homework\" nov 20");
 		list = controller.getCurrentList();
-	
-		assertEquals(getDesc(list.get(0)),"**No search result**");
+		controller.execCmd("delete 1 2 3 4 5 6");
+		assertEquals(getDesc(list.get(0)),"4. do homework and sleep");
+		
+		
 	}
-	
-	
-	
-	
-	
 	
 	
 }
